996300285547fe75e0e53ddf7733f569854a54c8
Roman <roman@devops.com>
2026-03-11 02:58:37 +0000

    fix: specify to terraform which version of helm to use in helm.tf

--- infrastructure/terraform/helm.tf
+++ infrastructure/terraform/helm.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
+  }
+}
+
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint

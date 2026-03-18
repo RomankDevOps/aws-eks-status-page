760f839f74247698671ef50e3fa5adb22349c139
Roman <roman@devops.com>
2026-03-18 15:29:59 +0000

    fix: correct template directory path to statuspage/templates

--- app/statuspage/settings.py
+++ app/statuspage/settings.py
@@ -398,3 +398,4 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'templates')]
+TEMPLATES[0]['DIRS'] = [os.path.join(BASE_DIR, 'statuspage', 'templates')]
